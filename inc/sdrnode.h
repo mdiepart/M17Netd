08c5e138c3fff8a3a48507e4480fe156f8fb6c90
Morgan Diepart <morgandiepart@gmail.com>
2024-09-04 15:33:15 +0200

    Added SDRNode driver class

---
+++ inc/sdrnode.h
@@ -0,0 +1,141 @@
+#pragma once
+
+#include <string>
+#include <cstdlib>
+#include <vector>
+
+#include <alsa/asoundlib.h>
+
+#include "sx1255.h"
+
+using namespace std;
+
+class sdrnode
+{
+    private:
+    // GPIOs
+    static constexpr unsigned gpio_ADC_temp_enable = 11;
+    static constexpr unsigned gpio_ADC_batt_enable = 12;
+    static constexpr unsigned gpio_PA_enable       = 15;
+    static constexpr unsigned gpio_TX_lowpower     = 16;
+    static constexpr unsigned gpio_bias_enable     = 17;
+    static constexpr unsigned gpio_relay_TX        = 55;
+    static constexpr size_t   sysfs_gpio_max_len   = 30;
+    static constexpr const char *sysfs_gpio_val    = "/sys/class/gpio/gpio%d/value";
+
+    /**
+     * Sets the level of a gpio pin
+     * 
+     * @param gpio pin number whoe level is to be set
+     * @param value value to set: true for high, false for low
+     * 
+     * @return 0 on success, -1 on error
+     */
+    int gpio_set_level(unsigned gpio, bool value);
+
+    // PCM/I2S
+    static constexpr unsigned ideal_rate   = 96000;
+    unsigned int pcm_rate = ideal_rate;
+    static constexpr const char *audio_rx_dev     = "hw:0,0"; // Or i2s0?
+    static constexpr const char *audio_tx_dev     = "hw:0,0"; // Or i2s0?
+    // We isolate pcm_access here to ease up moving to mmap mode
+    static constexpr snd_pcm_access_t pcm_access = SND_PCM_ACCESS_RW_INTERLEAVED;
+    snd_pcm_t *pcm_hdl;
+
+    /**
+     * Open the PCM device in TX mode
+     * 
+     * @return 0 on success, -1 on error
+     */
+    int open_pcm_tx();
+
+    /**
+     * Open the PCB device in RX mode
+     * 
+     * @return 0 on success, -1 on error
+     */
+    int open_pcm_rx();
+
+    /**
+     * Closes the PCM device. 
+     */
+    void close_pcm();
+    
+
+    // SX1255
+    static constexpr const char *spi_devname = "/dev/spidev1.0";
+    sx1255_drv sx1255 = sx1255_drv(spi_devname);
+
+    /**
+     * Sets the GPIOs ready for TX
+     */
+    inline void prepare_tx()
+    {
+        gpio_set_level(gpio_relay_TX, 1);
+
+        if( usleep(10000) < 0)
+            cerr << "usleep was interrupted while preparing to TX." << endl;
+        
+        //gpio_set_level( gpio_PMAX_BUFA_enable, 1 ); // ??
+        gpio_set_level( gpio_PA_enable, 1 );
+        gpio_set_level( gpio_bias_enable, 1 );
+    }
+
+    /**
+     * Sets the GPIOs ready for RX
+     */
+    inline void prepare_rx()
+    {
+        gpio_set_level( gpio_bias_enable, 0 ) ;
+        gpio_set_level( gpio_PA_enable, 0 ) ;
+        gpio_set_level( gpio_relay_TX, 0 ) ;
+    }
+
+    // Internal state
+    bool tx_nRx = false;
+    unsigned long rx_frequency = 0;
+    unsigned long tx_frequency = 0;
+
+
+    public:
+    sdrnode(const unsigned long rx_freq, const unsigned long tx_freq);
+
+    /**
+     * Switches the sdrnode unit in RX mode. Once in RX mode, the samples can be querried with receive()
+     * 
+     * @return 0 on success, -1 on error
+     */
+    int switch_rx();
+
+    /**
+     * Switches the sdrnode unit in TX mode. Once in TX mode, samples can be provided with transmit()
+     * 
+     * @return 0 on success, -1 on error
+     */
+    int switch_tx();
+
+    /**
+     * Reads at most n samples from the radio
+     * This operation will read a number n of samples. Each 
+     * sample containing two floats, it will read at most 2n floats
+     * 
+     * @param rx buffer containing 2n floats
+     * @param n number of I/Q samples to send. One sample is composed of 2 floats
+     * 
+     * @return the number of samples read
+     */
+    size_t receive(float *rx, const size_t n);
+
+    /**
+     * Writes at most n samples to the radio
+     * This operation will write a number n of samples. Each sample containing
+     * two floats, it will write at most 2n floats.
+     * 
+     * @param tx buffer containing 2n floats
+     * @param n number of I/Q samples to send. One sample is composed of 2 floats
+     * 
+     * @return the number of samples written
+     */
+    size_t transmit(const float *tx, const size_t n);
+
+};
---
+++ src/sdrnode.cpp
@@ -0,0 +1,443 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <alsa/asoundlib.h>
+
+#ifdef __aarch64__
+#include <arm_neon.h>
+#endif
+
+#include "spi.h"
+#include "sdrnode.h"
+
+/**
+ * Convert an array of n float to an array of n 16 bits signed integers
+ * 
+ * Uses ARM Neon extension if available
+ */
+void float_to_int16(float *input, int16_t *output, const size_t n);
+
+/**
+ * Convert an array of n 16 bits fixed point signed integers to an array of floats
+ * 
+ * Uses ARM Neon extension if available
+ */
+void int16_to_float(int16_t *input, float *output, const size_t n);
+
+
+void float_to_int16(float *input, int16_t *output, const size_t n)
+{
+#ifdef __aarch64__
+    int16x4_t out;
+    int32x4_t tmp;
+    float32x4_t in;
+
+    const size_t simd_iters = n/4; // How many iters we can do with simd (4 floats per iter)
+    const size_t remainder_iters = n%4; // How many iters are left if n is not a multiple of 4
+
+    // Loop over the input buffer, convert floats 4 by 4 using SIMD
+    for(size_t i = 0; i < simd_iters; i++)
+    {
+        in = vld1q_f32(input); // Load next 4 floats
+        __builtin_prefetch(input+4, 0, 0); // Pre-fetch the next 4 floats, we read, lowest temporal locality.
+        tmp = vcvtq_n_s32_f32(in, 16); // Convert float to 16 bits fixed point (stored in 32 bits int).
+        out = vmovn_s32(tmp); // narrows down the 32 bits to 16 bits integers.
+        vst1_s16(output, out); // Store the converted number in output array.
+        __builtin_prefetch(output+4, 1, 0); // Pre-fetch the next 4 int16, we write, lowest temporal locality.
+        input += 4; // Increment input pointer
+        output += 4; // Increment output pointer
+    }
+
+    for(size_t i = 0; i < remainder_iters; i++)
+    {
+        // Scale to 16 bits, cast and store
+        *output = static_cast<int16_t>((*input) * __INT16_MAX__);
+        input++;
+        output++;
+
+    }
+#else
+    // Naïve implementation, manually convert all floats to 16 bits integers
+
+    for(size_t i = 0; i < n; i++)
+    {
+        // Scale to 16 bits, cast and store
+        *output = static_cast<int16_t>((*input) * __INT16_MAX__);
+        input++;
+        output++;
+    }
+
+#endif
+}
+
+void int16_to_float(int16_t *input, float *output, const size_t n)
+{
+#ifdef __aarch64__
+    float32x4_t out;
+    int32x4_t tmp;
+    int16x4_t in;
+
+    const size_t simd_iters = n/4; // How many iters we can do with simd (4 floats per iter)
+    const size_t remainder_iters = n%4; // How many iters are left if n is not a multiple of 4
+
+    // Loop over the input buffer, convert floats 4 by 4 using SIMD
+    for(size_t i = 0; i < simd_iters; i++)
+    {
+        in = vld1_s16(input); // Load next 4 16-bits fixed points integers
+        __builtin_prefetch(input+4, 0, 0); // Pre-fetch the next 4 ints, we read, lowest temporal locality.
+        tmp = vmovl_s16(in); // Widen 16 bits to 32 bits integers
+        tmp = vcvtq_n_f32_s32(in, 16);// Convert 16 bits fixed point signed integer to float (scaled down by 1/__INT16_MAX__)
+        vst1q_f32(output, out); // Store the converted number in output array.
+        __builtin_prefetch(output+4, 1, 0); // Pre-fetch the next 4 int16, we write, lowest temporal locality.
+        input += 4; // Increment input pointer
+        output += 4; // Increment output pointer
+    }
+
+    for(size_t i = 0; i < remainder_iters; i++)
+    {
+        // Cast to float then scale down and store
+        *output = static_cast<float>(*input) / __INT16_MAX__;
+        input++;
+        output++;
+    }
+#else
+    // Naïve implementation, manually convert all floats to 16 bits integers
+
+    for(size_t i = 0; i < n; i++)
+    {
+        // Cast to float then scale down and store
+        *output = static_cast<float>(*input) / __INT16_MAX__;
+        input++;
+        output++;
+    }
+
+#endif
+}
+
+sdrnode::sdrnode(const unsigned long rx_freq, const unsigned long tx_freq)
+{
+    if(tx_freq < 400e6 || tx_freq > 510e6)
+        throw invalid_argument("TX frequency is outside the [400,510] MHz range.");
+
+    if(rx_freq < 400e6 || rx_freq > 510e6)
+        throw invalid_argument("RX frequency is outside the [400,510] MHz range.");
+
+    tx_frequency = tx_freq;
+    rx_frequency = rx_freq;
+
+    // Open PCM device for RX
+    open_pcm_rx();
+
+    // Set gpios for RX
+    prepare_rx();
+
+    // Switch SX1255 to RX mode
+    sx1255.switch_rx();
+
+    tx_nRx = false;
+}
+
+int sdrnode::gpio_set_level(unsigned gpio, bool value)
+{
+    int fd;
+    char gpio_filename[sysfs_gpio_max_len];
+
+    snprintf(gpio_filename, sysfs_gpio_max_len, sysfs_gpio_val, gpio);
+
+    fd = open(gpio_filename, O_WRONLY);
+    if (fd < 0) {
+        cerr << "gpioSetValue unable to open gpio " 
+             << to_string(gpio) << ": " << strerror(errno)
+             << "." << endl;
+        return -1;
+    }
+
+    if (value) {
+        if (write(fd, "1", 2) != 2) {
+            cerr << "gpioSetValue ON error : gpio "
+                 << to_string(gpio) << ": " << strerror(errno)
+                 << "." << endl;
+            close(fd);
+            return -1;
+        }
+    }
+    else {
+        if (write(fd, "0", 2) != 2) {
+            cerr << "gpioSetValue OFF error :gpio " 
+                 <<  to_string(gpio) << ": " << strerror(errno)
+                 << "." << endl;
+            close(fd);
+            return -1;
+        }
+    }
+    close(fd);
+    return 0;
+}
+
+int sdrnode::open_pcm_rx()
+{
+    int err;
+    snd_pcm_hw_params_t *pcm_hw_params;
+
+    err = snd_pcm_open(&pcm_hdl, audio_rx_dev, SND_PCM_STREAM_CAPTURE, 0);
+    if (err < 0) {
+        cerr << "Cannot open audio device " << audio_rx_dev
+             << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return -1;
+    }
+
+    snd_pcm_hw_params_malloc(&pcm_hw_params);
+    /*err = snd_pcm_hw_params_any(pcm_hdl, pcm_hw_params);
+    if (err < 0) {
+        cerr << "Cannot initialize hardware parameter structure: " 
+        << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }*/
+
+    err = snd_pcm_set_params(pcm_hdl, SND_PCM_FORMAT_S16,
+                             pcm_access, 2, ideal_rate, false,
+                             100000 /* default latency of 100ms*/);
+    if(err < 0)
+    {
+        cerr << "Cannot set PCM parameters: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return -1;
+    }                         
+
+    /*err = snd_pcm_hw_params_set_access(pcm_hdl, pcm_hw_params, pcm_access);
+    if (err < 0) {
+        cerr << "Cannot set access type: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    err = snd_pcm_hw_params_set_format(pcm_hdl, pcm_hw_params, SND_PCM_FORMAT_S16);
+    if (err < 0) {
+        cerr << "Cannot set sample format: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    pcm_rate = ideal_rate;
+    err = snd_pcm_hw_params_set_rate_near(pcm_hdl, pcm_hw_params, &pcm_rate, 0);
+    if (err < 0) {
+        cerr << "cannot set sample rate: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+    cout << "PCM audio device " << audio_rx_dev 
+         << ": exact sample rate set to "<< to_string(pcm_rate) << endl;
+
+    err = snd_pcm_hw_params_set_channels(pcm_hdl, pcm_hw_params, 2);
+    if (err < 0) {
+        cerr << "cannot set channel count: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    err = snd_pcm_hw_params(pcm_hdl, pcm_hw_params);
+    if (err < 0) {
+        cerr << "cannot set parameters: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }*/
+
+    cout << "pcm_hw_params set successfuly" << endl;
+
+    snd_pcm_hw_params_free(pcm_hw_params);
+
+    err = snd_pcm_prepare(pcm_hdl); 
+    if (err < 0) {
+        cerr << "cannot prepare audio interface for use: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return -1;
+    }
+
+    return 0;
+}
+
+int sdrnode::open_pcm_tx()
+{
+     int err;
+    snd_pcm_hw_params_t *pcm_hw_params;
+
+    err = snd_pcm_open(&pcm_hdl, audio_rx_dev, SND_PCM_STREAM_PLAYBACK, 0);
+    if (err < 0) {
+        cerr << "Cannot open audio device " << audio_rx_dev
+             << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    snd_pcm_hw_params_malloc(&pcm_hw_params) ;
+    /*err = snd_pcm_hw_params_any(pcm_hdl, pcm_hw_params);
+    if (err < 0) {
+        cerr << "Cannot initialize hardware parameter structure: " 
+        << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }*/
+
+    err = snd_pcm_set_params(pcm_hdl, SND_PCM_FORMAT_S16_LE,
+                             pcm_access, 2, ideal_rate, false,
+                             100000 /* default latency of 100ms*/);
+    if(err < 0)
+    {
+        cerr << "Cannot set PCM parameters: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }                         
+
+    /*err = snd_pcm_hw_params_set_access(pcm_hdl, pcm_hw_params, pcm_access);
+    if (err < 0) {
+        cerr << "Cannot set access type: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    err = snd_pcm_hw_params_set_format(pcm_hdl, pcm_hw_params, SND_PCM_FORMAT_S16_LE);
+    if (err < 0) {
+        cerr << "Cannot set sample format: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    pcm_rate = ideal_rate;
+    err = snd_pcm_hw_params_set_rate_near(pcm_hdl, pcm_hw_params, &pcm_rate, 0);
+    if (err < 0) {
+        cerr << "cannot set sample rate: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+    cout << "PCM audio device " << audio_rx_dev 
+         << ": exact sample rate set to "<< to_string(pcm_rate) << endl;
+
+    err = snd_pcm_hw_params_set_channels(pcm_hdl, pcm_hw_params, 2);
+    if (err < 0) {
+        cerr << "cannot set channel count: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    err = snd_pcm_hw_params(pcm_hdl, pcm_hw_params);
+    if (err < 0) {
+        cerr << "cannot set parameters: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }*/
+
+    cout << "pcm_hw_params set successfuly" << endl;
+
+    snd_pcm_hw_params_free(pcm_hw_params);
+
+    err = snd_pcm_prepare(pcm_hdl); 
+    if (err < 0) {
+        cerr << "cannot prepare audio interface for use: " << snd_strerror(err) << endl;
+        pcm_hdl = nullptr;
+        return err;
+    }
+
+    return 0;   
+}
+
+void sdrnode::close_pcm()
+{
+    if(pcm_hdl == nullptr)
+        return;
+
+    snd_pcm_drain(pcm_hdl);
+    snd_pcm_close(pcm_hdl);
+}
+
+int sdrnode::switch_rx()
+{
+    // Close pcm device and open it in rx
+    close_pcm();
+    int ret = open_pcm_rx();
+    if(ret < 0)
+        return -1;
+
+    prepare_rx(); // Configure GPIOs
+
+    ret = sx1255.switch_rx(); // Switch radio to RX mode
+    if(ret < 0)
+        return -1;
+
+    tx_nRx = false;
+
+    return 0;
+}
+
+int sdrnode::switch_tx()
+{
+    // Close pcm device and open it in tx
+    close_pcm();
+    int ret = open_pcm_tx();
+    if(ret < 0)
+        return -1;
+
+    prepare_tx(); // Configure GPIOs
+
+    ret = sx1255.switch_tx(); // Switch radio to RX mode
+    if(ret < 0)
+        return -1;
+
+    tx_nRx = true;
+
+    return 0;
+}
+
+size_t sdrnode::receive(float *rx, size_t n)
+{
+    // TODO use snd_pcm_mmap_readi to avoid an unnecessary malloc and copy
+    if(!tx_nRx)
+    {
+        // Check that we are in a state where we can receive data
+        int16_t *buff = new int16_t[n*2];
+        
+        if(buff == nullptr)
+            return 0;
+        snd_pcm_sframes_t read = snd_pcm_readi(pcm_hdl, buff, n);
+        
+        if(read <= 0)
+        {
+            delete[](buff);
+            return 0;
+        }
+        else
+        {
+            int16_to_float(buff, rx, (size_t)read*2);
+            delete[](buff);
+            return read;
+        }
+    }
+
+    return 0;
+}
+
+size_t sdrnode::transmit(const float *tx, size_t n)
+{
+    if(tx_nRx)
+    {
+        int16_t *buff = new int16_t[n*2];
+
+        if(buff == nullptr)
+            return 0;
+        
+        float_to_int16(const_cast<float*>(tx), buff, n*2);
+        snd_pcm_sframes_t written = snd_pcm_writei(pcm_hdl, buff, n);
+
+        delete[](buff);
+
+        return (written<=0)?0:written;
+    }
+
+    return 0;
+}

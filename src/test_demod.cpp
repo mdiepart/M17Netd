ae1d65b90860f1c2ace85e81292a0876bf0aced1
Morgan Diepart <morgandiepart@gmail.com>
2024-10-21 16:00:08 +0200

    Updated demodulation test

--- src/test_demod.cpp
+++ src/test_demod.cpp
@@ -33,30 +33,81 @@ int main(int argc, char *argv[])
     sigint_handler.sa_flags = 0;
     sigaction(SIGINT, &sigint_handler, 0);
 
-    ifstream iq_in_file("../iq_out.raw", ios_base::binary);
-
-    freqdem fdem = freqdem_create(0.0375);
-    static constexpr int32_t scale = (1 < 24);
-    M17Demodulator m17dem;
-
-    m17dem.init();
-    int32_t i, q;
-    uint64_t cnt = 0;
-    while( iq_in_file >> i >> q )
+    // Parse first argument (number of samples to acquire)
+    if(argc == 2 && strcmp(argv[1], "help") == 0)
     {
-        cnt++;
-        complex<float> iq_samp(static_cast<float>(i)/scale, static_cast<float>(q)/scale);
-        float msg_samp;
+        cout << "Usage: " << argv[0] << " kf iq_in_file\n"
+             << "\tkf           is frequency modulation index.\n"
+             << "\tiq_in_file   is the file from which to read the samples to feed to the demodulator.\n"
+             << endl;
+        return EXIT_SUCCESS;
+    }
+    else if(argc != 3)
+    {
+        cerr << "Incorrect usage, type \"" << argv[0] << " help\" to learn more." << endl;
+        return EXIT_FAILURE;
+    }
+
+    ifstream iq_in_file;
+    float kf = 0;
+
+    // Try to parse the fm index
+    try
+    {
+        kf = stof(argv[1]);
+    }
+    catch(const std::exception& e)
+    {
+        cerr << "Invalid frequency modulation index \"" << argv[1] << "\"." << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Try to open the output file
+    try
+    {
+        iq_in_file.open(argv[2], ios_base::binary);
+    }
+    catch(const std::exception &e)
+    {
+        cerr << "Unable to open output file \"" << argv[2] << "\"." << endl;
+        return EXIT_FAILURE;
+    }
+
+    freqdem fdem = freqdem_create(kf);
+    M17Demodulator m17dem;
+    m17dem.init();
+
+    float i, q;
+    uint64_t cnt = 0;
+
+    // Read I and Q (stored as floats)
+    iq_in_file.read(reinterpret_cast<char *>(&i), 4);
+    iq_in_file.read(reinterpret_cast<char *>(&q), 4);
+
+    float msg_samp;
+
+    while( iq_in_file.good() && running )
+    {
+        complex<float> iq_samp(i, q);
 
         freqdem_demodulate(fdem, iq_samp, &msg_samp);
+
         bool new_frame = m17dem.update(&msg_samp, 1);
 
         if(new_frame)
         {
             cout << "New frame." << endl;
+            (void)m17dem.getFrame();
         }
+
+        cnt++;
+
+        // Read I and Q (stored as floats)
+        iq_in_file.read(reinterpret_cast<char *>(&i), 4);
+        iq_in_file.read(reinterpret_cast<char *>(&q), 4);
     }
 
+    freqdem_destroy(fdem);
     iq_in_file.close();
 
     cout << "Read " << cnt << " samples." << endl;
